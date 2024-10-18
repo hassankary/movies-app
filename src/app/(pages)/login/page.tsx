78bb79af4fccf3b2b56f0d856d52427165a26c5f
Hassan Kaeru <hassanaskary29@gmail.com>
2024-10-18 13:34:38 +0700

    fix: login form z-index

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -4,9 +4,11 @@ export default function Login() {
   return (
     <div className="min-h-screen inset-0 pt-[60.8px] fixed container flex flex-col flex-grow mx-auto px-4 sm:px-0 xl:max-w-7xl">
       <div className="flex h-full items-center justify-center">
-        <div className="absolute animate-spin animate-duration-[3000ms] w-10 h-40 blur-2xl bg-red-500 rounded-full" />
-        <div className="absolute animate-spin animate-reverse animate-duration-[3000ms] w-10 h-40 blur-2xl bg-red-500 rounded-full" />
-        <LoginForm />
+        <div className="z-0 absolute animate-spin animate-duration-[3000ms] w-10 h-40 blur-2xl bg-red-500 rounded-full" />
+        <div className="z-0 absolute animate-spin animate-reverse animate-duration-[3000ms] w-10 h-40 blur-2xl bg-red-500 rounded-full" />
+        <div className="z-10">
+          <LoginForm />
+        </div>
       </div>
     </div>
   );
